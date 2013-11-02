6d6d5aa7a304faca10c3c2635e0c2d08199482d3
isglazunov <isglazunov@gmail.com>
2013-11-02 08:03:50 +0300

    Prototyping.

--- blackstone.js
+++ blackstone.js
@@ -17,6 +17,86 @@
         
         // Version
         blackstone.version = 'develop';
+        
+        // Prototyping
+        (function(prototyping){
+            prototyping.prototypesIndex = 0;
+            prototyping.instancesIndex = 0;
+            
+            // Prototype
+            var Prototype = prototyping.Prototype = function(options){
+                this.index = prototyping.prototypesIndex++;
+                this.constructor = undefined;
+                this.prototype = {};
+                this.prototypes = [];
+                lodash.merge(this, options);
+            };
+            
+            var _instanceof = function(prototype, index){
+                if(prototype instanceof Prototype) {
+                    if (prototype.index == index) return true;
+                    else {
+                        for (var p in prototype.prototypes) {
+                            if (_instanceof(prototype.prototypes[p].prototype, index)) return true;
+                        }
+                    }
+                }
+                return false;
+            };
+            
+            Prototype.prototype.new = function(){
+                var prototype = this;
+                
+                var pre = function(){};
+                pre.prototype = new Instance;
+                
+                // prototypes.prototypes
+                lodash.each(prototype.prototypes, function(ins){
+                    if (ins instanceof Instance && ins.prototype && ins.prototype.prototype)
+                        lodash.merge(pre.prototype, ins.prototype.prototype);
+                });
+                
+                // prototype.prototype
+                if (prototype.prototype) lodash.merge(pre.prototype, prototype.prototype);
+                
+                var instance = new pre;
+                
+                // prototypes instances
+                lodash.each(prototype.prototypes, function(ins){
+                    if (ins instanceof Instance) lodash.merge(instance, ins);
+                });
+                
+                instance.prototype = prototype;
+                
+                // constructor
+                if (lodash.isFunction(prototype.constructor)) prototype.constructor.apply(instance, arguments);
+                else if(lodash.isObject(prototype.constructor)) lodash.merge(instance, prototype.constructor);
+                
+                instance.index = prototyping.instancesIndex++;
+                
+                return instance;
+            };
+            
+            Prototype.prototype.instanceof = function(){
+                for (var a in arguments) {
+                    if (!_instanceof(this, arguments[a].index)) return false;
+                }
+                return true;
+            };
+            
+            // Instance
+            var Instance = prototyping.Instance = function(){};
+            
+            Instance.prototype.instanceof = function(){
+                for (var a in arguments) {
+                    if (arguments[a] instanceof Prototype) {
+                        if (!_instanceof(this.prototype, arguments[a].index)) return false;
+                    }
+                }
+                return true;
+            };
+            
+        }(blackstone.prototyping = {}))
     };
     
     // Version
---
+++ tests/prototyping/readme.md
@@ -0,0 +1,5 @@
+# The order of the test run
+
+bash:
+
+1. `mocha index.js -R spec`
