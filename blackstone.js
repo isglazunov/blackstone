9264c3863f0fe422b7d1c3d2e6c23106d8021abb
isglazunov <isglazunov@gmail.com>
2013-11-23 02:45:59 +0300

    0.0.4

--- blackstone.js
+++ blackstone.js
@@ -1,4 +1,4 @@
-// blackstone@0.0.3
+// blackstone@0.0.4
 // https://github.com/isglazunov/blackstone
 
 // Help
@@ -10,7 +10,7 @@
     
     // Blackstone Version
     // Version for internal use
-    var __version = '0.0.3';
+    var __version = '0.0.4';
     
     // new (lodash, async)
     // Main constructor
